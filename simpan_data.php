f663de840ce22d2b08985c712963b4435781d810
rdadesukro <rdade30@gmail.com>
2021-11-16 08:04:15 +0700

    new

--- simpan_data.php
+++ simpan_data.php
@@ -1,28 +1,37 @@
 <?php
-
+if($_SERVER['REQUEST_METHOD']=='POST') {
   require_once 'koneksi.php'; 
-  header('Content-Type: application/json');
-
+  
 
    $response = array();
-   $nama = addslashes(trim($_POST['nama']));
-	 $alamat = addslashes(trim($_POST['alamat']));
+   $nama = trim($_POST['nama']);
+	 $alamat = trim($_POST['alamat']);
 
    //var_dump($nama." ".$id);
    
    
-$query1 = "INSERT INTO `nama_tamus` (`id`, `users_id`, `nama`, `alamat`, `uuid`, `created_at`, `updated_at`, `baca`) VALUES (NULL, '1', 'ade', 'ade', '23213', '2021-07-01 11:45:46', '2021-07-01 11:45:46', '1');";
+$query1 = "INSERT INTO `nama_tamus` (`id`, `nama`, `alamat`, `uuid`) VALUES (NULL, '$nama', '$alamat', 'qe3')";
 
 	          
-  
-     if(mysqli_query($konek,$query1)) {
-       $response["kode"] = "1";
-       $response["message"] = "Sukses Ganti Nomor";
-       echo json_encode($response,JSON_PRETTY_PRINT);
+     if(mysqli_query($connect,$query1)) {
+       $response = array(
+        'status' => 1,
+        'message' =>'Success');           
      } else {
-       $response["kode"] = "0";
-       $response["message"] = "gagal edit data";
-       echo json_encode($response,JSON_PRETTY_PRINT);
+      $response = array(
+        'status' => 0,
+        'message' =>'Gagal');        
      }
 	
-       mysqli_close($konek);
+      // mysqli_close($connect);
+
+} else {
+  $response = array(
+    'status' => 0,
+    'message' =>'Method salah');      
+}
+header('Content-Type: application/json');
+echo json_encode($response)
+?>
+
+
